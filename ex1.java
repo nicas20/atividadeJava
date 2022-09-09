860d190dfc090cf2b4b33616344bbfbd74878f27
nicas20 <100000527+nicas20@users.noreply.github.com>
2022-09-08 21:39:07 -0300

    Update ex1.java

--- ex1.java
+++ ex1.java
@@ -1,35 +1,21 @@
+package atividadeJava;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class ex1 {
 
     public static void main(String[] args) {
-
-        int maior = 0;
-        int menor = 0;
-
-        int[] valor = new int[5];
         Scanner sc = new Scanner(System.in);
-
-        for (int i = 0; i < 5; i++) {
-            System.out.println("Insira o valor " + i);
-            valor[i] = sc.nextInt();
-
-            if (maior < valor[i]) {
-                maior = valor[i];
-            }
-            if (i == 0) {
-                menor = valor[1];
-
-            }
-            if (menor > valor[i]) {
-                menor = valor[i];
-            }
-
+        int numeros[] = new int[5];
+        for (int i = 0; i < numeros.length; i++) {
+            System.out.println("Insira o " + (i + 1) + "º número: ");
+            numeros[i] = sc.nextInt();
         }
-        System.out.println("a maior e " + maior);
-
-        System.out.println("O menor é " + menor);
+        Arrays.sort(numeros);
+        for (int i = 0; i < numeros.length; i++) {
+            System.out.println("Numeros[" + i + "] = " + numeros[i]);
+        }
+        sc.close();
     }
-
 }
