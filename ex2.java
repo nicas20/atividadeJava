938b55196019a9d5e1da651bceefa743f4ec5589
nicas20 <100000527+nicas20@users.noreply.github.com>
2022-09-08 21:41:24 -0300

    Update ex2.java

--- ex2.java
+++ ex2.java
@@ -1,16 +1,24 @@
+package atividadeJava;
+
+import java.text.DecimalFormat;
+import java.util.Scanner;
 
 public class ex2 {
 
     public static void main(String[] args) {
-        double sal = 1000;
-        int ano = 1996;
-        int atual = 2022;
-        int dif = atual - ano;
-        double perc = 1.5;
-        for (int i = 1; i <= dif; i++) {
-            sal = sal + (sal * perc) / 100;
-            perc *= 2;
-        }
-        System.out.println("O salario atual é: R$" + sal);
+        Scanner sc = new Scanner(System.in);
+        DecimalFormat df = new DecimalFormat("0.00");
+        double salario[] = new double[2];
+        salario[0] = 1000;
+        salario[1] = salario[0] + (0.015 * salario[0]);
+        int ano = 1997;
+        double taxa = 0.015;
+        do {
+            taxa = taxa * 2;
+            salario[1] = salario[1] + (taxa * salario[1]);
+            ano = ano + 1;
+        } while (ano != 2023);
+        System.out.println("O salário em " + (ano - 1) + " anos é: R$" + df.format(salario[1]));
+        sc.close();
     }
 }
